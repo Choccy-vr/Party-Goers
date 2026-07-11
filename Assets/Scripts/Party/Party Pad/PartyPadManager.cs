4c8852dd885c4a6d8a67100d02687f040d0eca5b
Ginobeano <94711504+Choccy-vr@users.noreply.github.com>
2026-07-11 16:22:37 -0400

    feat(Party Pad): current turn screen

---
+++ Assets/Scripts/Party/Party Pad/PartyPadManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PartyPadManager : MonoBehaviour
+{
+    public static PartyPadManager Instance { get; private set; }
+
+    [SerializeField] GameObject currentTurnScreen;
+    [SerializeField] GameObject testScreen;
+    [SerializeField] GameObject playerStanding;
+
+    GameObject currentActiveScreen;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    static void ResetStatics()
+    {
+        Instance = null;
+    }
+
+    void setActiveScreen(GameObject screen)
+    {
+        if (currentActiveScreen != null)
+        {
+            currentActiveScreen.SetActive(false);
+        }
+        screen.SetActive(true);
+        currentActiveScreen = screen;
+    }
+
+    public void setCurrentPlayerTurnUI()
+    {
+        setActiveScreen(currentTurnScreen);
+    }
+    public void setTestUI()
+    {
+        setActiveScreen(testScreen);
+    }
+    public void setPlayerStandingOverlay(bool isActive)
+    {
+        playerStanding.SetActive(isActive);
+    }
+}
